9df72f2eb89dbba721f15c05aa6f0cf344323dd1
ray <geemguang@gmail.com>
2019-04-17 22:22:33 +0900

    some

--- EXPERIMENT_RUN/a3c_small_domain/PPPPPPLLLLLLLOOOOOOTTTTTTT/competing_loss_max_panelty_lambda_find/logs/plot_a3c_log.py
+++ EXPERIMENT_RUN/a3c_small_domain/PPPPPPLLLLLLLOOOOOOTTTTTTT/competing_loss_max_panelty_lambda_find/logs/plot_a3c_log.py
@@ -46,13 +46,19 @@ def xy(name):
         miss.append(mi)
         mass.append(ma)
     return lss, miss, mass
-name = '20190410-14-26-18-WM_competing_loss_lambda_0.5.txt'
+# name = '20190410-14-26-18-WM_competing_loss_lambda_0.5.txt'
 # name = '20190410-14-26-38-WM_competing_loss_lambda_0.7.txt'
 # name = '20190410-14-26-49-WM_competing_loss_lambda_0.2.txt'
-name = '20190410-15-14-30-WM_competing_loss_lambda_0.85_more_E.txt'
-# name = '20190410-15-14-12-WM_competing_loss_lambda_0.75_more_E.txt'
-name = '20190410-16-14-07-WM_competing_loss_more_layer_lambda_0.7.txt'
-name = '20190410-16-15-28-WM_competing_loss_more_layer_lambda_0.85.txt'
+# name = '20190410-15-14-30-WM_competing_loss_lambda_0.85_more_E.txt'
+name = '20190410-15-14-12-WM_competing_loss_lambda_0.75_more_E.txt'
+# name = '20190410-16-14-07-WM_competing_loss_more_layer_lambda_0.7.txt'
+# name = '20190410-16-15-28-WM_competing_loss_more_layer_lambda_0.85.txt'
+# name = '20190411-11-12-22-CL_maxlim_l0.9_m_20_c_50.txt'  # **
+# name = '20190411-11-12-43-CL_maxlim_l0.9_m_20_c_50_2.txt'  #**
+# name = '20190411-11-12-51-CL_maxlim_l0.9_m_20_c_50_3.txt'  #**
+# name = '20190411-11-44-12-CL_maxlim_l0.9_m_20_c_50_4.txt'
+# name = '20190411-11-44-26-CL_maxlim_l0.9_m_20_c_50_5.txt'
+# name = '20190411-11-45-03-CL_maxlim_l0.9_m_100_c_50_1.txt'
 l, i, x = xy(name)
 # print(l)
 
@@ -66,9 +72,9 @@ def plot_this(file_name, plot_name):
 # plt.plot(x, y)
 # plt.scatter(t_log[0], t_log[1])
 fig, ax = plt.subplots()
-ax.plot(l, label='loss')
-ax.plot(i, label='mi loss')
-ax.plot(x, label='ma loss')
+# ax.plot(l, label='loss')
+ax.plot(i, label='minimizing loss', color='r')
+ax.plot(x, label='maximizing loss', color='b')
 # plot_this('3ss.txt', '3 Simp')
 # plot_this('max.txt', 'Max1')
 # plot_this('rnn.txt', 'RNN')
@@ -79,7 +85,7 @@ ax.plot(x, label='ma loss')
 # plot_this('vae_rnn.txt', 'VAE')
 ax.grid(True)
 ax.legend(loc='upper left')
-ax.set_title('competing loss, More Layer, lambda=0.85')
+ax.set_title('Discriminative Loss, lambda=0.9, M=20')
 ax.set_xlabel('epoch')
 ax.set_ylabel('loss value')
 # ax.set_xlim(left=0, right=50000000*1)
